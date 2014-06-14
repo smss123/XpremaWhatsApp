39b573d7a98fbd33ed7e646b88aad49a67b52234
smss123 <smsssamer@live.com>
2014-06-14 09:54:36 -0700

    Create User Class

---
+++ XpremaWhatApp/WhatsAppDataLayer/Tables/History.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsAppDataLayer.Tables
+{
+    public class History
+    {
+        public int ID { get; set; }
+        public string  ActionName { get; set; }
+        public DateTime  ActionTime { get; set; }
+        public string  Description { get; set; }
+        public User  user { get; set; }
+    }
+}
---
+++ XpremaWhatApp/WhatsAppDataLayer/Tables/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsAppDataLayer.Tables
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string  FullName { get; set; }
+        public string  PhoneNumber { get; set; }
+        public string  UserName { get; set; }
+        public string  Password { get; set; }
+        public bool  IsAdmin { get; set; }
+        public bool  IsSend { get; set; }
+        public bool  IsWhatsAppIDSender { get; set; }
+        public bool  IsNumberManage { get; set; }
+        public virtual List<History> UserHistory { get; set; }
+    }
+}
