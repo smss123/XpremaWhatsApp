94aec4d17aef3d52550c1f37b4fd5ff3f5b97459
smss123 <smsssamer@live.com>
2014-06-14 09:25:59 -0700

    Review

---
+++ XpremaWhatApp/Xprema Xaina/SendPanal/frmReviewIDSender.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Xprema_Xaina.SendPanal
+{
+    partial class frmReviewIDSender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IDSender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Checker = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.lblNumbers = new System.Windows.Forms.Label();
+            this.lblIDSenderCounter = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IDSender,
+            this.Checker});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(236, 417);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // IDSender
+            // 
+            this.IDSender.FillWeight = 152.2843F;
+            this.IDSender.HeaderText = "ID Sender";
+            this.IDSender.Name = "IDSender";
+            // 
+            // Checker
+            // 
+            this.Checker.FillWeight = 47.71573F;
+            this.Checker.HeaderText = "Status";
+            this.Checker.Name = "Checker";
+            // 
+            // lblNumbers
+            // 
+            this.lblNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumbers.Location = new System.Drawing.Point(364, 85);
+            this.lblNumbers.Name = "lblNumbers";
+            this.lblNumbers.Size = new System.Drawing.Size(98, 20);
+            this.lblNumbers.TabIndex = 2;
+            this.lblNumbers.Text = "lbl lbl";
+            // 
+            // lblIDSenderCounter
+            // 
+            this.lblIDSenderCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblIDSenderCounter.Location = new System.Drawing.Point(364, 56);
+            this.lblIDSenderCounter.Name = "lblIDSenderCounter";
+            this.lblIDSenderCounter.Size = new System.Drawing.Size(98, 20);
+            this.lblIDSenderCounter.TabIndex = 3;
+            this.lblIDSenderCounter.Text = "lbl lbl";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(254, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tatal Reviewed";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(254, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID Sender Counter";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(674, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Review";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmReviewIDSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(761, 417);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblNumbers);
+            this.Controls.Add(this.lblIDSenderCounter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmReviewIDSender";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Review IDSender";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDSender;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Checker;
+        private System.Windows.Forms.Label lblNumbers;
+        private System.Windows.Forms.Label lblIDSenderCounter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
--- XpremaWhatApp/Xprema Xaina/SendPanal/frmSendPanal.Designer.cs
+++ XpremaWhatApp/Xprema Xaina/SendPanal/frmSendPanal.Designer.cs
@@ -29,27 +29,34 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblCurrnetSend = new System.Windows.Forms.Label();
+            this.lblNumbers = new System.Windows.Forms.Label();
+            this.lblIDSenderCounter = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.NumberlistBox = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.progressBar1);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblCurrnetSend);
+            this.groupBox1.Controls.Add(this.lblNumbers);
+            this.groupBox1.Controls.Add(this.lblIDSenderCounter);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label1);
@@ -59,6 +66,68 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "information";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(320, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Value = 30;
+            // 
+            // lblCurrnetSend
+            // 
+            this.lblCurrnetSend.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCurrnetSend.Location = new System.Drawing.Point(116, 74);
+            this.lblCurrnetSend.Name = "lblCurrnetSend";
+            this.lblCurrnetSend.Size = new System.Drawing.Size(98, 20);
+            this.lblCurrnetSend.TabIndex = 0;
+            this.lblCurrnetSend.Text = "lbl lbl";
+            // 
+            // lblNumbers
+            // 
+            this.lblNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumbers.Location = new System.Drawing.Point(116, 45);
+            this.lblNumbers.Name = "lblNumbers";
+            this.lblNumbers.Size = new System.Drawing.Size(98, 20);
+            this.lblNumbers.TabIndex = 0;
+            this.lblNumbers.Text = "lbl lbl";
+            // 
+            // lblIDSenderCounter
+            // 
+            this.lblIDSenderCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblIDSenderCounter.Location = new System.Drawing.Point(116, 16);
+            this.lblIDSenderCounter.Name = "lblIDSenderCounter";
+            this.lblIDSenderCounter.Size = new System.Drawing.Size(98, 20);
+            this.lblIDSenderCounter.TabIndex = 0;
+            this.lblIDSenderCounter.Text = "lbl lbl";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(6, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Currnt Send";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(6, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tatal Number";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Sender Counter";
             // 
             // statusStrip1
             // 
@@ -82,90 +151,88 @@
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
             this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
             // 
-            // label1
+            // NumberlistBox
             // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ID Sender Counter";
+            this.NumberlistBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.NumberlistBox.FormattingEnabled = true;
+            this.NumberlistBox.Location = new System.Drawing.Point(0, 0);
+            this.NumberlistBox.Name = "NumberlistBox";
+            this.NumberlistBox.Size = new System.Drawing.Size(168, 369);
+            this.NumberlistBox.TabIndex = 2;
             // 
-            // label2
+            // btnSend
             // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(116, 16);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "lbl lbl";
+            this.btnSend.Location = new System.Drawing.Point(631, 343);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send ";
+            this.btnSend.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // groupBox2
             // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(6, 45);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 20);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Tatal Number";
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(370, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(199, 99);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ID Sender";
             // 
-            // label4
+            // checkBox1
             // 
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label4.Location = new System.Drawing.Point(116, 45);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 20);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "lbl lbl";
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(10, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Select All ID Sender";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // checkBox2
             // 
-            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label5.Location = new System.Drawing.Point(6, 74);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 20);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Currnt Send";
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(10, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(106, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Select ID Sender";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // numericUpDown1
             // 
-            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label6.Location = new System.Drawing.Point(116, 74);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 20);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "lbl lbl";
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(10, 100);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(320, 23);
-            this.progressBar1.TabIndex = 1;
-            this.progressBar1.Value = 30;
-            // 
-            // listBox1
-            // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(168, 369);
-            this.listBox1.TabIndex = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(10, 65);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1486618625,
+            232830643,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 6;
             // 
             // frmSendPanal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(718, 391);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.NumberlistBox);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmSendPanal";
-            this.Text = "frmSendPanal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Send Panal";
             this.groupBox1.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,13 +244,18 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblIDSenderCounter;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNumbers;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblCurrnetSend;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox NumberlistBox;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
